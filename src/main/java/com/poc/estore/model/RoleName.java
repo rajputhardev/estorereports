7a2bfee2d1e0b6f0856d6b2c5dfba0a57844112f
rajputhardev <rajput.hardev@gmail.com>
2019-12-09 02:29:11 +0530

    first commit

---
+++ src/main/java/com/poc/estore/model/RoleName.java
@@ -0,0 +1,7 @@
+package com.poc.estore.model;
+
+
+public enum  RoleName {
+    ROLE_USER,
+    ROLE_ADMIN
+}
---
+++ src/main/java/com/poc/estore/repository/RoleRepository.java
@@ -0,0 +1,14 @@
+package com.poc.estore.repository;
+
+import com.poc.estore.model.Role;
+import com.poc.estore.model.RoleName;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(RoleName roleName);
+}
